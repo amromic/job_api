5267f0c656b29b779f4e7ab1274b3382ef283418
Amoora <a.m.r.o.s@homail.com>
2018-10-06 04:18:15 +0000

    fix some issues in index page part2

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,6 @@
 class Job < ApplicationRecord
 
-validates :title, presence: true, length:{minimum:3,maximum:30}
-validates :description, presence: true , length:{minimum:10, maximum:500}
+validates :title, presence: true, length:{minimum:3,maximum:50}
+validates :description, presence: true , length:{minimum:10, maximum:10000}
 
 end
